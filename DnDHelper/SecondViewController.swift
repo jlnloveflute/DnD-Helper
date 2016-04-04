03eb81e62fdb0b721ac5ed3422a50e065098b58c
Rita Fang <rfang@princeton.edu>
2016-04-03 22:54:45 -0400

    Initial Commit

---
+++ DnDHelper/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DnDHelper
+//
+//  Created by Rita Fang on 4/3/16.
+//  Copyright © 2016 Rita Fang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
